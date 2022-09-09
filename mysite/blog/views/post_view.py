019bd2f24a2ccd6a4d6bc1ed1bb58c02df77b7f4
pdr3392 <pchenr1que1@gmail.com>
2022-09-09 19:58:15 -0300

    forms almost done

--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,5 +1,7 @@
 from django.views import generic
+from blog.forms import CommentForm
 from blog.models import Post
+from django.shortcuts import get_object_or_404, render
 
 
 class PostView(generic.ListView):
@@ -7,6 +9,38 @@ class PostView(generic.ListView):
     template_name = 'index.html'
 
 
-class PostDetail(generic.DetailView):
+""" class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+ """
+
+
+def post_detail(request, slug):
+    template_name = 'post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True).order_by('-created_on')
+    new_comment = None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+
+            new_comment.post = post
+            new_comment.save()
+
+    else:
+        comment_form = CommentForm()
+
+    return render(
+        request,
+        template_name,
+        {
+            'post': post,
+            'comments': comments,
+            'new_comment': new_comment,
+            'comment_form': comment_form,
+        }
+    )
